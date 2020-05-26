f19bf10d6d06a065b0fc7ea4d2be252aa9670771
jchen <jchen.6727@gmail.com>
2020-05-25 22:50:50 -0400

    Working cfg and netParams
    Now appropriate traces
    netParams now instantiating cells after using
    "cellType" and "cellModel"

--- cfg.py
+++ cfg.py
@@ -3,9 +3,10 @@ from netpyne import specs
 
 cfg = specs.SimConfig()  
 
-#simulation parameters
-cfg.dt = 0.0125
-cfg.cvode_active = False
+#simulation parameters (fixed timestep)
+#cfg.dt = 0.0125
+#cfg.cvode_active = False
+cfg.cvode_active = True
 cfg.recordStims = False
 cfg.recordStep = 1
 
@@ -13,6 +14,7 @@ cfg.recordStep = 1
 cfg.freq = 25
 cfg.npulses = 10
 cfg.amp = 0.3
+cfg.dur = 5
 cfg.mttxs = 1.0
 cfg.mn1p8 = 1.0
 
@@ -21,16 +23,16 @@ cfg.recordCells = ['all']
 for label, chan in [ ['NaV1.7', 'nattxs'], ['NaV1.8', 'nav1p8'] ]:
     cfg.recordTraces[label] = {'sec': 'soma', 'loc': 0.5, 'var': 'ina_%s' %(chan)}
 
-pts = 5
+pts = 3
 #generate recordTraces along the fiber and at soma (ordered dictionary so generate them in order)
 for x in range(-pts, 0):
     x /= pts
-    cfg.recordTraces['v_fiber(%+1.1f)' %(0-x)] = {'sec': 'peri', 'loc': (1-x), 'var': 'v'}
+    cfg.recordTraces['v_fiber(%+1.2f)' %(x)] = {'sec': 'peri', 'loc': (1+x), 'var': 'v'}
 
 for x in range(0, pts+1):
     x /= pts
-    cfg.recordTraces['v_fiber(%+1.1f)' %(x  )] = {'sec': 'cntr', 'loc': x    , 'var': 'v'}
-
+    cfg.recordTraces['v_fiber(%+1.2f)' %(x)] = {'sec': 'cntr', 'loc': x    , 'var': 'v'}
+    
 cfg.recordTraces['v_soma'] = {'sec': 'soma', 'loc': 0.5, 'var': 'v'}
 
 # Saving
@@ -44,7 +46,7 @@ cfg.saveDataInclude = ['simData']
 cfg.hParams = {'celsius': 22, 'v_init': -53.5}
 cfg.duration = 1000/cfg.freq * cfg.npulses + 50
 
-cfg.analysis.plotTraces = {'include': ['all'], 'overlay': True, 'oneFigPer': 'cell', 'saveData': True, 'saveFig': 'plots/n7_%.1f_n9_%.3f_k2_%.3f_k3_%.3f.png' %(),
+cfg.analysis.plotTraces = {'include': ['all'], 'overlay': True, 'oneFigPer': 'cell', 'saveData': True, 'saveFig': True,
                            'showFig': False, 'timeRange': [0, cfg.duration]}
 
 #use the saveData to plot values
--- netParams.py
+++ netParams.py
@@ -12,21 +12,29 @@ netParams= specs.NetParams()   # object of class NetParams to store the network 
 # set up current clamp definitions, NetStim and IPClamp point process
 nslbl = 'Stim(%dHz)' %(freq)
 iplbl = 'IPC(%.1fnAx%.1fms)' %(dur, amp)
-netParams.stimSourceParams[nslbl] = {'cellModel': 'NetStim', 'rate': freq, 'noise': 0, 'start': 15, 'number': npulses}
+netParams.stimSourceParams[nslbl] = {'type': 'NetStim', 'rate': freq, 'noise': 0, 'start': 15, 'number': npulses}
 netParams.synMechParams[iplbl] = {'mod': 'IPClamp', 'dur': dur, 'amp': amp}
 # create unique tag strings for soma and tjunction
 tjlbl= 'tjcnrn'
 solbl= 'socnrn'
 
 # load cell designs
-tjRules= netParams.importCellParams(label= tjlbl, conds={'cell': tjlbl}, fileName= 'cells.py', cellName= 'npTJ'  , cellArgs= {'mulnattxs': mttxs, 'mulnav1p8': mn1p8})
-soRules= netParams.importCellParams(label= solbl, conds={'cell': solbl}, fileName= 'cells.py', cellName= 'npSoma', cellArgs= {'mulnattxs': mttxs, 'mulnav1p8': mn1p8})
+tjRules= netParams.importCellParams(label= tjlbl, conds={'cellType': tjlbl, 'cellModel': tjlbl}, fileName= 'cells.py', cellName= 'npTJ'  , cellArgs= {'mulnattxs': mttxs, 'mulnav1p8': mn1p8})
+soRules= netParams.importCellParams(label= solbl, conds={'cellType': solbl, 'cellModel': solbl}, fileName= 'cells.py', cellName= 'npSoma', cellArgs= {'mulnattxs': mttxs, 'mulnav1p8': mn1p8})
+
 # assign cell parameters
 netParams.cellParams[tjlbl]= tjRules
 netParams.cellParams[solbl]= soRules
 # assign cells to populations
-netParams.popParams[tjlbl]= {'numCells': 1, 'cell': tjlbl}
-netParams.popParams[solbl]= {'numCells': 1, 'cell': solbl}
+netParams.popParams[tjlbl]= {'numCells': 1, 'cellType': tjlbl, 'cellModel': tjlbl}
+netParams.popParams[solbl]= {'numCells': 1, 'cellType': solbl, 'cellModel': solbl}
 # attach current pulse to cells ( to socnrn->soma(0.5), tjcnrn->peri(0.0) )
-netParams.stimTargetParams['%s->%s%s' %(nslbl, iplbl, solbl)] = {'source': nslbl, 'target': {'cell': solbl}, 'sec': 'soma', 'loc': 0.5, 'weight': 1, 'delay': 5, 'synMech': iplbl}
-netParams.stimTargetParams['%s->%s%s' %(nslbl, iplbl, tjlbl)] = {'source': nslbl, 'target': {'cell': tjlbl}, 'sec': 'peri', 'loc': 0.0, 'weight': 1, 'delay': 5, 'synMech': iplbl}
+netParams.stimTargetParams['%s->%s%s' %(nslbl, iplbl, solbl)] = {'source': nslbl, 'conds': {'pop': solbl}, 'sec': 'soma', 'loc': 0.5, 'weight': 1, 'delay': 5, 'synMech': iplbl}
+netParams.stimTargetParams['%s->%s%s' %(nslbl, iplbl, tjlbl)] = {'source': nslbl, 'conds': {'pop': tjlbl}, 'sec': 'peri', 'loc': 0.0, 'weight': 1, 'delay': 5, 'synMech': iplbl}
+
+if __name__=='__main__':
+    from pprint import pprint
+    print('--SOMA--')
+    pprint(soRules)
+    print('---TJ---')
+    pprint(tjRules)
