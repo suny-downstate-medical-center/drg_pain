b3b41e4bc960d1aee61333e0ae7c7d6a22d33345
James Chen <jchen.6727@gmail.com>
2020-06-05 21:33:48 -0400

    bash nav channels

--- run_1p8.sh
+++ run_1p8.sh
@@ -1,22 +1,5 @@
 #!bin/bash
 
-#cfg.freq = CFGFREQ
-#cfg.npulses = CFGNPULSES
-#cfg.amp = CFGAMP
-#cfg.dur = CFGDUR
-#cfg.mttxs = CFGMTTXS
-#cfg.mn1p8 = CFGMN1P8
-#cfg.mn1p9 = CFGMN1P9
-#cfg.simLabel = 'CFGSIMLABEL'
-
-
-#CFGFREQ='5'
-NPULSES='30'
-AMP='0.3'
-DUR='5'
-MTTXS='1.0'
-MN1P8='1.0'
-MN1P9='1.0'
 #create appropriate tmp python file
 echo "generating freqtmp.py file"
 for CNDCT in 1.0 1.2 1.4 1.6 1.8 2.0
@@ -25,7 +8,7 @@ do
     for FREQ in 25 23 21 19 17 15 13 11 9 7 5
     do
         echo "TESTING: cndct: ${CNDCT}, freq: ${FREQ} Hz"
-        sed "s/CFGFREQ/${FREQ}/; s/CFGSIMLABEL/mn1p9${CNDCT}freq${FREQ}" mn1p9tmp > cfg.py
+        sed "s/CFGFREQ/${FREQ}/; s/CFGSIMLABEL/mn1p9${CNDCT}freq${FREQ}" mn1p8tmp > cfg.py
         python init.py
     done
 done
