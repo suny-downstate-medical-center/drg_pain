a2840160d3a33c6c9d6a934d9c537a759155696c
James Chen <jchen.6727@gmail.com>
2020-06-05 21:46:59 -0400

    still fixing 1 character to transfer file

--- run_freq.sh
+++ run_freq.sh
@@ -6,7 +6,7 @@ bash gen_tmp.sh 30 0.3 5 1.0 1.0 1.0 freqtmp
 for freq in 25 23 21 19 17 15 13 11 9 7 5
 do
     echo "freq: $freq Hz"
-    sed "s/CFGFREQ/$freq/" freqtmp | sed "s/CFGSIMLABEL/freq$freq" > cfg.py
+    sed "s/CFGFREQ/$freq/" freqtmp | sed "s/CFGSIMLABEL/freq$freq/" > cfg.py
     python init.py
 done
 #clean up
