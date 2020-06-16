c88598b8069d71c565f242ec75dd91994cd5ad89
James Chen <jchen.6727@gmail.com>
2020-06-16 16:46:23 -0400

    wip data

--- data.py
+++ data.py
@@ -7,13 +7,31 @@ import pickle
 import numpy as np
 import re
 
+_lfc = {
+    'pkl': lambda fp: pickle.load(fp)
+    'json': lambda fp: json.load(fp)
+}
+
 class data():
     def __init__(self, output = 'analysis/', delim = '_'):
         self.output = output
         self.data = {}
         self.delim = delim
-        self.regex = {}
 
+    def load_data(self, filename, filetype = None, prepend = None):
+        if not prepend:
+            prepend = "%s%s" %(filename)
+        if not filetype:
+            filetype = re.split('.', filename)[1]
+        try:
+            _lfc[filetype](filename)
+                data
+        except:
+            return False
+        data = _lfc[filetype](fp)
+        self.load_simData(data)
+        return True
+        
     def load_pkl(self, filename, prepend = None):
         if not prepend:
             prepend = "%s%s" %(filename)
@@ -22,6 +40,10 @@ class data():
                 data = pickle.load(fp)
         except:
             return False
+        self.load_simData(data) 
+        return True
+
+    def load_simData(self, data):
         self.data[prepend] = data
         for key in data['simData']:
             if isinstance(data['simData'][key], dict):
@@ -31,10 +53,10 @@ class data():
                     self.data['%s%s%s%s%s%s%s' %(prepend, self.delim, pop, self.delim, cellid, self.delim, key)] = data['simData'][key][cell]
             else:
                 self.data['%s%s%s' %(prepend, self.delim, key)] = self.data['simData'][key]
-        return True
-
-    def create_regroup(self):
-        re.compile()
+        
     def group_traces(self, gfunction):
-        for
-
+        group = {}
+        for key in self.data:
+            if gfunction(key):
+                group[key] = self.data[key]
+        return group
