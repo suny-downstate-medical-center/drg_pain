c68544ad72f5718b025b0b70927a97b4d9f11698
jchen <jchen.6727@gmail.com>
2020-10-30 06:27:35 -0400

    testbench for clamps

---
+++ test.py
@@ -0,0 +1,51 @@
+from neuron import h
+
+###
+: sine pulse driven by event.
+
+NEURON {
+	POINT_PROCESS SinClamp
+	RANGE dur, i, amp, t0
+	ELECTRODE_CURRENT i
+}
+
+UNITS {
+	(nA) = (nanoamp)
+  PI = (pi) (1)
+}
+
+PARAMETER {
+	dur = 1 (ms)	
+	amp = 0.1 (nA)
+}
+
+ASSIGNED {
+	i (nA)
+	toff (ms)
+  t0 (ms)
+}
+
+INITIAL {
+	i = 0
+  toff = -1
+}
+
+NET_RECEIVE(weight (uS)) {
+  t0 = t
+	toff = t + dur
+}
+
+BREAKPOINT {
+  at_time(t0)
+  at_time(toff)
+  i = 0
+  if (t0 < t && t < toff) {
+    i = amp * sin(PI * ((t - t0) / dur))
+  }
+}
+###
+sec = h.Section(name=sec)
+sine = h.SinClamp(sec(0.5))
+
+sine.dur = 1
+sine.amp = 0.1
