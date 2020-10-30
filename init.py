c52067e4972692b57f232a59b51a07fb7c278978
jchen <jchen.6727@gmail.com>
2020-10-30 13:47:05 -0400

    init with clamp waveforms

--- init.py
+++ init.py
@@ -2,5 +2,29 @@ from netpyne import sim
 
 cfg, netParams = sim.readCmdLineArgs()
 sim.create(simConfig = cfg, netParams = netParams)
+
+
 sim.simulate()
 sim.analyze()
+
+
+
+# Create network and run simulation
+sim.create(netParams = netParams, simConfig = simConfig)
+...
+netParams.stimSourceParams['iclamp'] = {'type': 'IClamp', 'amp': 0.0, 'dur': 1000, 'delay': 0}
+netParams.stimTargetParams['iclamp->PYR'] = {'source': 'iclamp', 'conds': {'pop': 'S'}, 'sec': 'soma', 'loc': 0.5}
+...
+init_amp = 0.0
+peak_amp = 0.24
+ramp_up = np.linspace(init_amp, peak_amp, simConfig.duration/(simConfig.dt))
+t = h.Vector(np.arange(0,simConfig.duration, simConfig.dt))
+amp = h.Vector(ramp_up)
+for cell in sim.net.cells:
+    try:
+        amp.play(cell.stims[0]['hObj']._ref_amp, t, True)
+    except:
+        pass
+sim.simulate()
+sim.analyze()
+
